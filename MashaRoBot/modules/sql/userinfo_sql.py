a25ece33271b1f1e0764ca16a02921d509a4e8df
kanesh546 <60382363+kanesh546@users.noreply.github.com>
2022-01-21 10:06:52 +0530

    Update userinfo_sql.py

--- MashaRoBot/modules/sql/userinfo_sql.py
+++ MashaRoBot/modules/sql/userinfo_sql.py
@@ -1,12 +1,12 @@
 import threading
 
 from MashaRoBot.modules.sql import BASE, SESSION
-from sqlalchemy import Column, Integer, UnicodeText
+from sqlalchemy import Column, Biginteger, UnicodeText
 
 
 class UserInfo(BASE):
     __tablename__ = "userinfo"
-    user_id = Column(Integer, primary_key=True)
+    user_id = ColumnBiginteger, primary_key=True)
     info = Column(UnicodeText)
 
     def __init__(self, user_id, info):
@@ -19,7 +19,7 @@ class UserInfo(BASE):
 
 class UserBio(BASE):
     __tablename__ = "userbio"
-    user_id = Column(Integer, primary_key=True)
+    user_id = Column(Biginteger, primary_key=True)
     bio = Column(UnicodeText)
 
     def __init__(self, user_id, bio):
